00e2aa6d6fbe05293b73f20619155daa64415a77
Marcin Pawlicki <marcinpawlicki0@gmail.com>
2024-05-12 17:57:38 +0200

    push lib

---
+++ lib/assets/images/index.d.ts
@@ -0,0 +1,4 @@
+/**
+ * src/images contains all the images used in the project.
+ */
+export declare const helloWorld = "hello world!";
---
+++ lib/translations/index.d.ts
@@ -0,0 +1,9 @@
+declare const _default: {
+    en: {
+        "hello.world": string;
+    };
+    pl: {
+        "hello.world": string;
+    };
+};
+export default _default;
