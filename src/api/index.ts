330ea8654e3ae914ec619233b7868ca568531e7c
Marcin Pawlicki <marcinpawlicki0@gmail.com>
2024-05-14 21:10:44 +0200

    fix: export types

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,3 +2,4 @@ export * from './libs/axiosClient';
 export { queryClient, clearQueryCache } from './libs/query';
 
 export * from './surveys/surveys.hooks';
+export * from './surveys/surveys.types';
