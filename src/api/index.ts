5f240cc3183d6cdb84edaae0f404ab14050e420c
Mikołaj Wiśniewski <javadreamsmikolaj@gmail.com>
2024-06-05 16:53:35 +0200

    fix: provider export

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,4 @@
-export * from './libs/axiosClient';
-export { queryClient, clearQueryCache, TanstackQueryProvider } from './libs/query';
-
-export * from './surveys/surveys.hooks';
-export * from './surveys/surveys.types';
+export * from './libs/axiosClient'
+export { queryClient, clearQueryCache, TanstackQueryProvider } from './libs/query'
+export * from './surveys/surveys.hooks'
+export * from './surveys/surveys.types'
