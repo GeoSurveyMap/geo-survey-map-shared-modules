c8867da630b9431873531822fa140926ae43a06c
Marcin Pawlicki <marcinpawlicki0@gmail.com>
2024-11-24 18:28:25 +0100

    fix: update exports

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,3 +2,4 @@ export * from './libs/axiosClient';
 export { queryClient, clearQueryCache, TanstackQueryProvider } from './libs/query';
 export * from './surveys/surveys.hooks';
 export * from './surveys/surveys.types';
+export * from './user/user.hooks';
