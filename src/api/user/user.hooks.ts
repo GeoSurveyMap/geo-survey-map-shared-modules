0f5d7f5b682160673409a55ea97c5b59d14b2dfd
Marcin Pawlicki <marcinpawlicki0@gmail.com>
2024-11-27 22:13:39 +0100

    fix: invalidate user surveys on login/register

--- src/api/user/user.hooks.ts
+++ src/api/user/user.hooks.ts
@@ -1,8 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 import { postRegisterUser } from './user';
+import { queryClient, queryKeys } from 'api/libs/query';
 
 export const useRegisterUser = () => {
   return useMutation({
     mutationFn: postRegisterUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [queryKeys.survey.user] });
+    },
   });
 };
