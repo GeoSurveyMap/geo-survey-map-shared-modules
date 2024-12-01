de506a22f92e0d5da1d8d3ac55d7833c026373ac
Mikołaj Wiśniewski <javadreamsmikolaj@gmail.com>
2024-12-01 22:48:41 +0100

    fix: tweak search params

--- src/api/ipinfo/ipinfo.ts
+++ src/api/ipinfo/ipinfo.ts
@@ -2,7 +2,7 @@ import { axiosClient, updateApiClient } from '../libs/axiosClient'
 import { getConnectionInfoResponse } from './ipinfo.types'
 
 export const getConnectionInfo = (token: string) => {
-	const params = new URLSearchParams(token)
-	updateApiClient.setBaseURL('https://ipinfo.io?')
-	return axiosClient.get<getConnectionInfoResponse>(params.toString())
+	const params = new URLSearchParams({ token })
+	updateApiClient.setBaseURL(`https://ipinfo.io?${params.toString()}`)
+	return axiosClient.get<getConnectionInfoResponse>('')
 }
