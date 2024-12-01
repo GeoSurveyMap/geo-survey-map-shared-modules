bd47deba0d0123ac59e2734cd33cbe979776e917
Mikołaj Wiśniewski <javadreamsmikolaj@gmail.com>
2024-12-01 22:07:42 +0100

    fix: add getCountry hook exports

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,3 +4,5 @@ export * from './surveys/surveys.hooks';
 export * from './surveys/surveys.types';
 export * from './user/user.hooks';
 export * from './user/user.types';
+export * from './ipinfo/ipinfo.hooks';
+export * from './ipinfo/ipinfo.types';
