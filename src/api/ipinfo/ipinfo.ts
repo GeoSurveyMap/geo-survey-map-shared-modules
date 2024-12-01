135f42140b1efa16ac3aa017c7cc302bca9e2874
Mikołaj Wiśniewski <javadreamsmikolaj@gmail.com>
2024-12-01 23:39:58 +0100

    feat: remove ipinfo query

--- src/api/ipinfo/ipinfo.ts
+++
@@ -1,8 +0,0 @@
-import { axiosClient, updateApiClient } from '../libs/axiosClient'
-import { getConnectionInfoResponse } from './ipinfo.types'
-
-export const getConnectionInfo = (token: string) => {
-	const params = new URLSearchParams({ token })
-	updateApiClient.setBaseURL(`https://ipinfo.io?${params.toString()}`)
-	return axiosClient.get<getConnectionInfoResponse>('')
-}
