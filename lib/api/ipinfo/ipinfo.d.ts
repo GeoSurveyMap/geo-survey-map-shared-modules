f082342e52453a21e5e07ba38237d90b627d13c3
Mikołaj Wiśniewski <javadreamsmikolaj@gmail.com>
2024-12-01 21:50:19 +0100

    fix: add getCountry hook build

---
+++ lib/api/ipinfo/ipinfo.d.ts
@@ -0,0 +1,2 @@
+import { getConnectionInfoResponse } from './ipinfo.types';
+export declare const getConnectionInfo: (token: string) => Promise<import("axios").AxiosResponse<getConnectionInfoResponse, any>>;
---
+++ lib/api/ipinfo/ipinfo.hooks.d.ts
@@ -0,0 +1,1 @@
+export declare function useGetCountry(token: string): import("@tanstack/react-query").UseQueryResult<string, Error>;
