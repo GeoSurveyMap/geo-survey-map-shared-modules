0bc9b64f568340baf936310d50e68805258db0c0
Marcin Pawlicki <marcinpawlicki0@gmail.com>
2024-12-06 19:32:12 +0100

    feat: add svg gms icon

--- lib/api/index.d.ts
+++ lib/api/index.d.ts
@@ -4,5 +4,3 @@ export * from './surveys/surveys.hooks';
 export * from './surveys/surveys.types';
 export * from './user/user.hooks';
 export * from './user/user.types';
-export * from './ipinfo/ipinfo.hooks';
-export * from './ipinfo/ipinfo.types';
