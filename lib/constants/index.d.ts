7f59ed9c3a8aa75716172f53905d40cb1d71c519
Mikołaj Wiśniewski <javadreamsmikolaj@gmail.com>
2025-01-18 00:23:07 +0100

    feat: add translations

--- lib/constants/index.d.ts
+++ lib/constants/index.d.ts
@@ -1,5 +1,8 @@
 import { Category } from '../types';
-export declare const problemCausesForCategories: Record<Category, string[]>;
+export declare const problemCausesForCategories: Record<Category, {
+    value: string;
+    translationKey: string;
+}[]>;
 export declare const categoryInformationPhoto: Record<Category, {
     uri: string | null;
     source: string;
